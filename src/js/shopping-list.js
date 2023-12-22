1c3eeb6452a3ad2ae8a5c975006712a152f5dcbf
Anna0067 <129055045+Anna0067@users.noreply.github.com>
2023-12-22 17:34:36 +0100

    img and styles added

---
+++ src/js/shopping-list.js
@@ -0,0 +1,4 @@
+// <img class="books.png" src="/src/images/books.png" alt="books" /> />
+const image = document.querySelector('.books-img');
+image.src = '/src/images/books.png';
+image.alt = 'Books';
