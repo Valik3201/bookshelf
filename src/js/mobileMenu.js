5bfca2302c6a669991927df69ba0f5dc7fd6dc4b
tomekstanczak <tomek.stanczak87@gmail.com>
2023-12-26 17:37:52 +0100

    correction

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -3,30 +3,7 @@ import { fetchBooks } from './bookAPI.js';
 const openMenuButton = document.querySelector('.open-mobile-menu');
 const closeMenuButton = document.querySelector('.close-button');
 const mobileMenuField = document.querySelector('.mobile-menu-field');
-const mobileMenuBackground = document.querySelector('.background-cover-container');
 const windowSize = window.matchMedia('(min-width: 768px)');
-const categoryName = 'Series Books';
-
-const displayBooksByCategory = async categoryName => {
-  const booksByCategory = await fetchBooks('category', categoryName);
-
-  if (!booksByCategory || !Array.isArray(booksByCategory)) {
-    console.error('Invalid response from fetchBooks:', booksByCategory);
-    return;
-  }
-
-  const booksMarkup = booksByCategory
-    .flat()
-    .map(book => {
-      return `<div class="background-cover">
-          <img src="${book.book_image}" alt="${book.title}">
-          </div>
-      `;
-    })
-    .join('');
-
-  mobileMenuBackground.insertAdjacentHTML('beforeend', booksMarkup);
-};
 
 const openMenu = () => {
   mobileMenuField.style.display = 'block';
