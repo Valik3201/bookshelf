85ffddc77a3d6b2accf347fb770b1d50ffed9cdd
tomekstanczak <tomek.stanczak87@gmail.com>
2023-12-26 09:34:14 +0100

    mobile menu disappearing

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,6 +1,7 @@
 const openMenuButton = document.querySelector('.open-mobile-menu');
 const closeMenuButton = document.querySelector('.close-button');
 const mobileMenuField = document.querySelector('.mobile-menu-field');
+const windowSize = window.matchMedia('(min-width: 768px)');
 
 const openMenu = () => {
   mobileMenuField.style.display = 'block';
@@ -17,5 +18,12 @@ const closeMenu = () => {
   }, 500);
 };
 
+function mobileMenuClose(event) {
+  if (event.matches) {
+    mobileMenuField.style.display = 'none';
+  }
+}
+
 openMenuButton.addEventListener('click', openMenu);
 closeMenuButton.addEventListener('click', closeMenu);
+windowSize.addEventListener('change', mobileMenuClose);
