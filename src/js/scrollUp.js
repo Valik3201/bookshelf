75d240f6952d74376f19b1ce4dbe9d7dc411d517
patryq03 <138616976+patryq03@users.noreply.github.com>
2024-01-03 11:51:49 +0100

    scroll up
    oddaje zrobionego scroll upa

--- src/js/scrollUp.js
+++ src/js/scrollUp.js
@@ -1,3 +1,17 @@
+const button = document.querySelector("#scrollUp");
+
 function scrollUp() {
   window.scrollTo(0, 0);
 }
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
+
+
+button.addEventListener("click", scrollUp);
+document.addEventListener("scroll" ,scrollFunction);
