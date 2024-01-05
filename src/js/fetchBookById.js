47e1b13f85d0a22273f65a95e6b3341613224a27
Valik <valik3201@gmail.com>
2024-01-05 10:22:35 +0100

    refactor: refactor book addition function

--- src/js/fetchBookById.js
+++ src/js/fetchBookById.js
@@ -1,12 +1,6 @@
 import { fetchBooks } from './bookAPI.js';
-import {
-  addToLocalStorage,
-  removeFromLocalStorage,
-  checkIfBookInLocalStorage,
-  showCongratulatoryMessage,
-  hideCongratulatoryMessage,
-} from './localStorage.js';
 import { modalContainer, toggleModal } from './modalHandler.js';
+import { bookAddButtonHandler } from './handleBookAddition.js';
 
 /**
  * Display book details by ID in a modal pop-up.
@@ -81,42 +75,5 @@ export const displayBookById = async bookId => {
   // Insert the modal markup into the modal container
   modalContainer.innerHTML = markup;
 
-  // Adding event handling for the "Add to Shopping List" buttons
-  const addToShoppingListButtons = document.querySelectorAll('.modal-pop-up-btn');
-  addToShoppingListButtons.forEach(function (button) {
-    const bookId = button.getAttribute('data-book-id');
-
-    // Checking if the book is already in localStorage
-    const isBookInLocalStorage = checkIfBookInLocalStorage(bookId);
-
-    if (isBookInLocalStorage) {
-      // If the book is in localStorage, change the button text
-      button.textContent = 'Remove from the shopping list';
-      // Add the congratulatory message
-      showCongratulatoryMessage();
-    } else {
-      // If the book is not in localStorage, add it and change the button text
-      hideCongratulatoryMessage();
-    }
-
-    // Adding click event listener with  function
-    button.addEventListener('click', function () {
-      // Checking if the book is already in localStorage
-      const isBookInLocalStorage = checkIfBookInLocalStorage(bookId);
-
-      if (isBookInLocalStorage) {
-        // If the book is in localStorage, remove it and change the button text
-        removeFromLocalStorage(bookId);
-        button.textContent = 'Add to shopping list';
-        // Remove the congratulatory message
-        hideCongratulatoryMessage();
-      } else {
-        // If the book is not in localStorage, add it and change the button text
-        addToLocalStorage(bookId);
-        button.textContent = 'Remove from the shopping list';
-        // Add the congratulatory message
-        showCongratulatoryMessage();
-      }
-    });
-  });
+  bookAddButtonHandler();
 };
--- src/js/handleBookAddition.js
+++ src/js/handleBookAddition.js
@@ -1,1 +1,48 @@
-import { addToLocalStorage } from './localStorage.js';
+import {
+  addToLocalStorage,
+  removeFromLocalStorage,
+  checkIfBookInLocalStorage,
+  showCongratulatoryMessage,
+  hideCongratulatoryMessage,
+} from './localStorage.js';
+
+export const bookAddButtonHandler = () => {
+  // Adding event handling for the "Add to Shopping List" buttons
+  const addToShoppingListButtons = document.querySelectorAll('.modal-pop-up-btn');
+  addToShoppingListButtons.forEach(function (button) {
+    const bookId = button.getAttribute('data-book-id');
+
+    // Checking if the book is already in localStorage
+    const isBookInLocalStorage = checkIfBookInLocalStorage(bookId);
+
+    if (isBookInLocalStorage) {
+      // If the book is in localStorage, change the button text
+      button.textContent = 'Remove from the shopping list';
+      // Add the congratulatory message
+      showCongratulatoryMessage();
+    } else {
+      // If the book is not in localStorage, add it and change the button text
+      hideCongratulatoryMessage();
+    }
+
+    // Adding click event listener with  function
+    button.addEventListener('click', function () {
+      // Checking if the book is already in localStorage
+      const isBookInLocalStorage = checkIfBookInLocalStorage(bookId);
+
+      if (isBookInLocalStorage) {
+        // If the book is in localStorage, remove it and change the button text
+        removeFromLocalStorage(bookId);
+        button.textContent = 'Add to shopping list';
+        // Remove the congratulatory message
+        hideCongratulatoryMessage();
+      } else {
+        // If the book is not in localStorage, add it and change the button text
+        addToLocalStorage(bookId);
+        button.textContent = 'Remove from the shopping list';
+        // Add the congratulatory message
+        showCongratulatoryMessage();
+      }
+    });
+  });
+};
