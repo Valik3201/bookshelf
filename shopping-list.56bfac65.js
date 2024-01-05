87f0d734e186756fdd0e0bb7850ad763ddb4d796
Valik3201 <valik3201@gmail.com>
2024-01-05 17:44:30 +0000

    Deploying to gh-pages from @ Valik3201/bookshelf@d6bdb18fd3a3de329ccf4fde1c53bc1795c63b53 🚀

--- shopping-list.56bfac65.js
+++
@@ -1,2 +0,0 @@
-var r=globalThis,e={},t={},i=r.parcelRequire0688;null==i&&((i=function(r){if(r in e)return e[r].exports;if(r in t){var i=t[r];delete t[r];var o={id:r,exports:{}};return e[r]=o,i.call(o.exports,o,o.exports),o.exports}var n=Error("Cannot find module '"+r+"'");throw n.code="MODULE_NOT_FOUND",n}).register=function(r,e){t[r]=e},r.parcelRequire0688=i),i.register,i("lthku");
-//# sourceMappingURL=shopping-list.56bfac65.js.map
