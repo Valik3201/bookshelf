86fb46f91d9514cfafba163db42dd9a1bd24a8c5
superfilar <mateusz9650@gmail.com>
2024-01-06 15:44:18 +0100

    update darkMode slider

--- src/js/darkMode.js
+++ src/js/darkMode.js
@@ -19,3 +19,25 @@
     }
   }
 })();
+
+(function () {
+  const toggler = document.querySelector('.switch input[id="t-switch-mobile-menu"]'),
+    root = document.documentElement,
+    currentTheme = localStorage.getItem('theme') || 'dark';
+
+  if (currentTheme == 'light') toggler.removeAttribute('checked');
+  else toggler.checked = 'true';
+  root.setAttribute('data-theme', currentTheme);
+
+  toggler.addEventListener('change', toggleTheme, false);
+
+  function toggleTheme(e) {
+    if (this.checked) {
+      root.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      root.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
+    }
+  }
+})();
